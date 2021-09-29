2f6c4dd3edbbddc8d12ad340bc466b3806bc6175
Andrea Powell <andreavpowell@gmail.com>
2021-09-28 18:34:14 -0700

    seeding to database

---
+++ seed.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+async function seed() {
+
+  await mongoose.connect(process.env.DATABASE_URL);
+
+  const Book = require('./models/BookSchema.js');
+
+    await Book.create({
+      title: 'Being and Nothingness',
+      description: 'An Essay on Phenomenological Ontology',
+      status: 'read',
+      email: 'bookreader1@gmail.com'
+    },
+    {
+      title: 'Ham on Rye',
+      description: 'Auto-biography',
+      status: 'unread',
+      email: 'charles@gmail.com'
+    },
+    {
+      title: 'Blink',
+      description: 'It presents in popular science format research from psychology and behavioral economics on the adaptive unconscious: mental processes that work rapidly and automatically from relatively little information.',
+      status: 'read',
+      email: 'bookworm@gmail.com'
+    },
+    {
+      title: 'Daring Greatly',
+      description: 'How the Courage to Be Vulnerable Transforms the Way We Live, Love, Parent, and Lead',
+      status: 'unread',
+      email: 'nerdygirl@yahoo.com'
+    }
+  );
+  mongoose.disconnect();
+}
+
+seed();
